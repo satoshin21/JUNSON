9d730d111f1590314a18bc0ce47e72bd632461a8
SatoshiN21 <satoshi.nagasaka@eure.jp>
2016-11-13 15:34:45 +0900

    add Carthage

---
+++ JUNSON-iOS/JUNSON-iOS.h
@@ -0,0 +1,19 @@
+//
+//  JUNSON-iOS.h
+//  JUNSON-iOS
+//
+//  Created by Satoshi Nagasaka on 2016/11/13.
+//  Copyright © 2016年 Satoshi Nagasaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JUNSON-iOS.
+FOUNDATION_EXPORT double JUNSON_iOSVersionNumber;
+
+//! Project version string for JUNSON-iOS.
+FOUNDATION_EXPORT const unsigned char JUNSON_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JUNSON_iOS/PublicHeader.h>
+
+
